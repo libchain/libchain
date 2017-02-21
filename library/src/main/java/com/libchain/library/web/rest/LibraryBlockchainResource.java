ad3d90051413deb957a05a21cbcd430edeb81d40
Gerrit Janßen <gerrit.janssen@mailbox.tu-berlin.de>
2017-02-21 10:46:58 +0100

    added Endpoints

--- library/src/main/java/com/libchain/library/web/rest/LibraryBlockchainResource.java
+++ library/src/main/java/com/libchain/library/web/rest/LibraryBlockchainResource.java
@@ -15,9 +15,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/blockchain/library")
 public class LibraryBlockchainResource {
 
-    @GetMapping("/borrow/{bookInstanceId}")
+    @GetMapping("/borrow/{bookInstanceId}/{pubkey}")
     @Timed
-    public ResponseEntity borrow(@PathVariable Long bookInstanceId) {
+    public ResponseEntity borrow(@PathVariable Long bookInstanceId, @PathVariable String pubkey) {
+        return new ResponseEntity(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @GetMapping("/books/all")
+    @Timed
+    public ResponseEntity listAllBooks() {
+        return new ResponseEntity(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @GetMapping("/statistics/allBooks")
+    @Timed
+    public ResponseEntity statisticsAll() {
+        return new ResponseEntity(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @GetMapping("/statistics/publisher/{publisherId}")
+    @Timed
+    public ResponseEntity statisticsPublisher(@PathVariable String publisherId) {
+        return new ResponseEntity(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @GetMapping("/statistics/book/{bookId}")
+    @Timed
+    public ResponseEntity statisticsBooks(@PathVariable String bookId) {
         return new ResponseEntity(HttpStatus.NOT_IMPLEMENTED);
     }
 
