decde49e31e79f2952ed8c26d1db5a5a91464ee1
juan <juanecabellob@gmail.com>
2017-03-07 15:19:05 +0100

    Fixes thrown error by migrating contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,6 @@
 var LibChain = artifacts.require("./LibChain.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(LibChain).then(function(instance) {
-    console.log(instance.address)
-
+  deployer.deploy(LibChain)
   });
 };
